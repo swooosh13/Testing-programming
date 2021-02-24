a1dec6925a94adbc8aa6033675b018a8e8d4be2e
Artyom Chernykh <artemon3162@gmail.com>
2021-02-24 20:17:18 +0300

    v3

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -2,10 +2,5 @@ package com.application;
 
 public class Main {
   public static void main(String[] args) {
-<<<<<<< master
-    System.out.println("dont stop me");
-=======
-    System.out.println("stop");
->>>>>>> master
   }
 }
